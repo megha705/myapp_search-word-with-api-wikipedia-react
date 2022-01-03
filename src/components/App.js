cb1989a9fbdcbd1c1b37907c3d12b5111c595a57
vida1375 <50640637+vida1375@users.noreply.github.com>
2022-01-03 22:00:06 +0330

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,27 @@
 import React from "react";
+import Info from "./Info";
 import Search from "./Search";
-import ImageList from "./ImageList";
-import axios from "axios";
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            images:[],
-            error:''
-        }
+const Data =[
+    {
+        title : 'what is React ?',
+        desc : 'React is a JS Library'
+    },
+    {
+        title : 'why React ?',
+        desc : 'Beacause it is fast and easy'
+    },
+    {
+        title : 'Is React available to use with Redux ?',
+        desc :'yes!'
     }
-    onSearchSubmit = (term) =>{
-        console.log(term)
-        axios.get('https://api.unsplash.com/search/photos',{
-            headers: {'Authorization': 'Client-ID xBL9w7KI3FjzwvjsNkYaRldH2ZK3jeq6uSKrEjR2kbI'},
-            params : {'query': term}
-        })
-        .then((response) => {
-         console.log(response)
-         this.setState({
-             images:response.data.results
-         })
-     })
-     .catch((error) => {
-         this.setState({
-             error : error
-         })
-     })
-    }
-    render(){
-        return(
-            <div>
-               <Search searchSubmit={this.onSearchSubmit} />
-               <ImageList images ={this.state.images} />
-            </div>
-        )
-    }
+]
+const App = () =>{
+    return(
+        <div>
+            <Search />
+            {/* <Info data ={Data} /> */}
+        </div>
+    )
 }
-
 export default App;
